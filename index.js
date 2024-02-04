ad40a24da1561ab1e1c88289f4f7139412b70a58
Connor Henn <connor.henn@yahoo.com>
2024-02-03 19:14:03 -0800

    final MVP

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+const navToggle = document.querySelector('.nav-toggle');
+const navList = document.querySelector('.nav__list');
+
+
+navToggle.addEventListener("click", () => {
+    navList.classList.toggle('hidden');
+})
